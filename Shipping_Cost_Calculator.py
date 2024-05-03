5c0d585e59c67f82d0b826fe404f559298649042
Leandra Magan <111862908+LeandraMT@users.noreply.github.com>
2024-05-03 10:24:10 +0200

    Delete Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++
@@ -1,9 +0,0 @@
-# Shipping Cost Calculator
-
-weight = float(input("Enter the package weight in kilograms: "))
-rate = float(input("Enter the shipping rate per kilogram: "))
-
-# Calculate the shipping cost
-shipping_cost = weight * rate
-
-print(f"shipping Cost {shipping_cost} USD")
